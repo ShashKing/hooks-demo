92b316c84c2184328380807f8119ee5572e9ed64
shashank <Shashankk165@gmail.com>
2020-05-20 20:47:00 +0530

    adding hooks

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounter2 from './components/HookCounter2';
+import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter/>
+      <HookCounter/> */}
+      {/* <HookCounter2/> */}
+      <HookCounter3/>
+      {/* <HookCounter4/> */}
+    </div>
+  );
+}
+
+export default App;
